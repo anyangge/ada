f8e7e4d48cc1eeb13e3b37dc82237ec084c4463f
graffio <graffio@lynxlab.com>
2014-04-08 15:49:06 +0200

    changed lenght of codice

--- modules/code_man/ada_code_man_module.sql
+++ modules/code_man/ada_code_man_module.sql
@@ -2,5 +2,5 @@
 #Run the following query on each provider of your ADA installation, excluding common
 #
 
-ALTER TABLE  `iscrizioni` ADD  `codice` VARCHAR( 7 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+ALTER TABLE  `iscrizioni` ADD  `codice` VARCHAR( 15 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
 ADD  `data_iscrizione` INT( 11 ) NOT NULL DEFAULT  '0';
